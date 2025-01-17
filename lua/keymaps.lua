1b78d6aa15eac81124c6d23e7dace446f2c00ccb
Zhicheng Yan <97499703+zhicyan@users.noreply.github.com>
2025-01-18 01:32:14 +0800

    Add custom configs for Netrw

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -45,4 +45,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- [[ Netrw Keymaps ]]
+-- Keymaps for entering the Netrw exploring mode
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
 -- vim: ts=2 sts=2 sw=2 et
--- lua/options.lua
+++ lua/options.lua
@@ -61,4 +61,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Hide Netrw banner
+vim.g.netrw_banner = 0
+
 -- vim: ts=2 sts=2 sw=2 et
