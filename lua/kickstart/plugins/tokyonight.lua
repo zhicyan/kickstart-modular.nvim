4105db4a3574c849f988e4e2a5e0b9fc07d9962a
Zhicheng Yan <97499703+zhicyan@users.noreply.github.com>
2025-01-18 11:31:38 +0800

    Add bg trans

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -14,6 +14,8 @@ return {
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
   },
 }
